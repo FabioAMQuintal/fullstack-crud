5af792e7126f41b076dc139d012c4a26d6922b32
Fabio Quintal <fabioamquintal@gmail.com>
2022-04-15 21:30:01 -0300

    delete method added

--- frontend/src/api/books.js
+++ frontend/src/api/books.js
@@ -1,4 +1,5 @@
 // TODO: Import API_ENDPOINT
+import { id } from "date-fns/locale";
 import BookSchedule from "../components/Booklist";
 import {API_ENDPOINT} from "./index"
 // TODO: Create the addNewBook function that takes in newTitle, newStart, and newEnd as arguments. Inside the function, create a POST request for the new book. Store the response as a JSON in a variable called newBook and return it at the end of the function.
@@ -40,3 +41,9 @@ export const updateBook = async (id, newTitle, newStart, newEnd) => {
     return response.status;
 }
 // TODO: Create the deleteBook function that takes the id of the book to be deleted as an argument. Inside of the function, create a DELETE request for the specified book to be deleted. Return the status of the response at the end of the function.
+export const deleteBook = async (id) => {
+    const response = fetch(`${API_ENDPOINT}/books/${id}`, {
+        method: 'DELETE'
+    })
+    return response.status;
+}
