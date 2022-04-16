c472ffbfbc31e9d966ce93b74f89630869357040
Fabio Quintal <fabioamquintal@gmail.com>
2022-04-15 21:28:22 -0300

    put method added

--- frontend/src/api/books.js
+++ frontend/src/api/books.js
@@ -1,4 +1,5 @@
 // TODO: Import API_ENDPOINT
+import BookSchedule from "../components/Booklist";
 import {API_ENDPOINT} from "./index"
 // TODO: Create the addNewBook function that takes in newTitle, newStart, and newEnd as arguments. Inside the function, create a POST request for the new book. Store the response as a JSON in a variable called newBook and return it at the end of the function.
 export const addNewBook = async (newTitle, newStart, newEnd) => {
@@ -18,7 +19,24 @@ export const addNewBook = async (newTitle, newStart, newEnd) => {
 
 }
 // TODO: Create the getBooks function that retrieves all of the books that have been saved to the back-end API
-
+export const getBooks = async () => {
+    const response = await fetch(`${API_ENDPOINT}/books`)
+    const book = await response.json()
+    return books;
+}
 // TODO: Create the updateBook function that takes the arguments id, newTitle, newStart, newEnd. Inside of the function, create a PUT request for the specified book to be updated. Return the status of the response at the end of the function.
-
+export const updateBook = async (id, newTitle, newStart, newEnd) => {
+    const response = fetch(`${API_ENDPOINT}/books/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+            title: newTitle,
+            start: newStart,
+            end: newEnd
+        }),
+        headers: {
+            "Content-type": "application/json"
+        }
+    })
+    return response.status;
+}
 // TODO: Create the deleteBook function that takes the id of the book to be deleted as an argument. Inside of the function, create a DELETE request for the specified book to be deleted. Return the status of the response at the end of the function.
